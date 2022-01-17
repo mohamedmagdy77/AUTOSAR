3d0a084bf7f60a9efb1f0d797314705c06efdee5
mohamedmagdy77 <39488559+mohamedmagdy77@users.noreply.github.com>
2022-01-17 04:39:36 +0200

    Update Det.c

--- Dio_Port&HAL/Det.c
+++ Dio_Port&HAL/Det.c
@@ -11,20 +11,14 @@
 
 #include "Det.h"
 
-uint16 GlobalModuleId = 0;
-uint8 GlobalInstanceId = 0;
-uint8 GlobalApiId = 0;
-uint8 GlobalErrorId = 0;
-
 Std_ReturnType Det_ReportError( uint16 ModuleId,
                                 uint8 InstanceId,
                                 uint8 ApiId,
                                 uint8 ErrorId )
 {
-    GlobalModuleId =   ModuleId ;
-    GlobalInstanceId = InstanceId;
-    GlobalApiId =      ApiId;
-    GlobalErrorId =    ErrorId; 
+    while(1){
+        
+    }
     return E_OK;
 }
 
